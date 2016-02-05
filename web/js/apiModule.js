ed7fd4212a1da5642a82d132ec0f65145145a848
Administrator <sebastien.tetard@yahoo.fr>
2016-02-05 21:44:18 +0100

    integration

--- web/js/apiModule.js
+++ web/js/apiModule.js
@@ -1,32 +1,46 @@
 var apiModule = (function($, Config) {
 
     var _baseUrl;
-
-    /**
-     * Get a gallery
-     *
-     * @param  {[type]} id [description]
-     * @return {[type]}    [description]
-     */
-    var loadGallery = function(id) {
-        var _serviceUrl = '/gallery/' + id;
-
-        return $.get(
-            _serviceUrl,
-            {'restrict': false}
-        );
+    var _templateMap = {
+        'gallery_single': '/gallery/{id}',
+        'gallery_edit_form': '/gallery/{id}/edit',
+        'gallery_add_form': '/gallery/add',
+        'gallery_add_address_form': '/gallery/address/add',
+        'gallery_list': '/gallery/list',
+        'connect_forgot_password_form': '/connect/forgot-password',
+        'connect_login_form': '/connect/login-form'
     };
 
     /**
-     * Load gallery edit form
+     * Load a twig template
      *
-     * @param  {[type]} id [description]
-     * @return {[type]}    [description]
+     * @param  {[type]} name template name
+     * @param  {[type]} opt  object that contains placeholders values
+     * @return promise
      */
-    var loadGalleryForm = function(id) {
-        var _serviceUrl = '/gallery/' + id + '/edit';
+    var loadTemplate = function(name, opt) {
+        if (_templateMap[name] !== undefined) {
+            var _serviceUrl = _templateMap[name];
+            var _matchs = _serviceUrl.match(/{([a-zA-Z])+}/gi);
 
-        return $.get(_serviceUrl);
+            if (_matchs) {
+                // replace placeholders with opt received as input
+                for (var i = 0; i < _matchs.length; i++) {
+                    var _parameter = _matchs[i].replace('{', '').replace('}', '');
+
+                    if (opt.hasOwnProperty(_parameter)) {
+                        _serviceUrl = _serviceUrl.replace(_matchs[i], opt[_parameter]);
+                    }
+                }
+            }
+
+            if (opt !== undefined
+                && opt.hasOwnProperty('queryString')) {
+                _serviceUrl += '?' + opt.queryString;
+            }
+
+            return $.get(_serviceUrl);
+        }
     };
 
     /**
@@ -44,6 +58,83 @@ var apiModule = (function($, Config) {
             type: 'PUT',
             url: _serviceUrl,
             data: data,
+            dataType: 'json',
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + btoa('artsper' + ":" + 'suih4igCd3mXP9r')
+            }
+        });
+    };
+
+    /**
+     * Add a gallery
+     * Sends a POST request to the API
+     *
+     * @param  {[type]} data [description]
+     * @return {[type]}      [description]
+     */
+    var addGallery = function(data) {
+        var _serviceUrl = _baseUrl + '/users/galleries';
+
+        return $.ajax({
+            type: 'POST',
+            url: _serviceUrl,
+            data: data,
+            dataType: 'json',
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + btoa('artsper' + ":" + 'suih4igCd3mXP9r')
+            }
+        });
+    };
+
+    /**
+     * Remove gallery address
+     *
+     * @param  {[type]} id_gallery
+     * @param  {[type]} id_address
+     * @return
+     * @todo /!\ THIS MUST BE SECURED when the JWT will be in place /!\
+     */
+    var deleteGalleryAddress = function(id_gallery, id_address) {
+        var _serviceUrl = _baseUrl
+            + '/users/galleries/'
+            + id_gallery
+            + '/addresses/'
+            + id_address;
+
+        return $.ajax({
+            type: 'DELETE',
+            url: _serviceUrl,
+            dataType: 'json',
+            crossDomain: true,
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Basic " + btoa('artsper' + ":" + 'suih4igCd3mXP9r')
+            }
+        });
+    };
+
+    /**
+     * Bulk operations on galleries
+     *
+     * @param  string action (archive|activate|inactivate)
+     * @param  JSON string data
+     * @return promise
+     */
+    var bulkGalleryAction = function(action, data) {
+        var _type = action == 'archive' ? 'DELETE' : 'POST';
+        var _serviceUrl = _baseUrl
+            + '/users/bulk/galleries'
+            + (action !== 'archive' ? '/' + action : '');
+
+        return $.ajax({
+            type: _type,
+            url: _serviceUrl,
+            data: data,
+            dataType: 'json',
             crossDomain: true,
             headers: {
                 "Content-Type": "application/json",
@@ -63,9 +154,11 @@ var apiModule = (function($, Config) {
 
     return {
         init: init,
-        loadGallery: loadGallery,
-        loadGalleryForm: loadGalleryForm,
-        editGallery: editGallery
+        loadTemplate: loadTemplate,
+        editGallery: editGallery,
+        addGallery: addGallery,
+        bulkGalleryAction: bulkGalleryAction,
+        deleteGalleryAddress: deleteGalleryAddress
     };
 
 })(jQuery, Config);
